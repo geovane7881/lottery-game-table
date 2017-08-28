353cc92644b29c91774e625e90d4ee3a2d098cf5
Geovane Clemente de Oliveira <geovane@localhost.localdomain>
2017-08-28 12:39:50 -0300

    filtrando e ordenando numeros

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
     <head>
     
-        <title>Vale Sorte</title>
+        <title>Cartelas Vale Sorte</title>
         <meta charset="utf-8">
         <link type="text/css" rel="stylesheet" href="css/estilo.css">
 
@@ -19,12 +19,12 @@
             <label>
                 Primeira Cartela: <br/>
                 <input type="text" id="numeros_1" name="numeros_1">
-            </label><br>
+            </label>
+            <br>
             <label>
                 Segunda Cartela: <br/>
                 <input type="text" id="numeros_2" name="numeros_2">
             </label>
-            
             <br><br>
             <button type="submit" id="btn_submit"> Enviar </button>
         </form>
--- tabela.php
+++ tabela.php
@@ -1,96 +1,79 @@
 <?php
-
 $numeros_1 = addslashes($_POST['numeros_1']);
 $numeros_2 = addslashes($_POST['numeros_2']);
+//tira ,,
+$numeros_1 = str_replace(',,', ',', $numeros_1);
+$numeros_2 = str_replace(',,', ',', $numeros_2);
+//tira ultima ,
+$numeros_1 = rtrim($numeros_1, ',');
+$numeros_2 = rtrim($numeros_2, ',');
 
 $tamanho = count(explode(',', $numeros_1)) ==  20 && count(explode(',', $numeros_2)) == 20 ? true : false;
-
     if(!$tamanho) {
         header('location: index.php');
     } 
-
 ?>
-
 <!doctype HTML>
 <html language="pt-br">
-
     <head>
-    
-        <title>Vale Sorte</title>
+        <title>Cartelas Vale Sorte</title>
         <meta charset="utf-8">
         <link type="text/css" rel="stylesheet" href="css/estilo.css">
-
         <script type="text/javascript">
-
             var numeros_1 = '<?php echo $numeros_1 ?>'.split(',');
             var numeros_2 = '<?php echo $numeros_2 ?>'.split(',');
-            
+            //ordena em ordem ascendente
+            numeros_1 = numeros_1.sort(function(a, b){return a-b});
+            numeros_2 = numeros_2.sort(function(a, b){return a-b});
 
-            function preenche_table(lista, tabela){
-                
+            function preenche_table(lista, tabela) {
                 var num = 0;
-                for(var linha=1; linha<=4; linha++){
-                    
+                for(var linha=0; linha<=3; linha++) {
                     var tr = document.createElement("tr");
                     tr.align = 'center';
                     tr.id = 'tr_'+tabela+'_'+linha;
                     //tr na table correspondente
                     document.getElementById('conteudo_tabela_'+tabela).appendChild(tr);
-
-                    for(var coluna=1; coluna<=5; coluna++){
-                        
+                    for(var coluna=0; coluna<=4; coluna++) {
                         var td = document.createElement('td');
                         td.id = 'num_'+tabela+'_'+num;
                         td.innerHTML = lista[num];
                         td.onclick = function(){ marca(this); }
-
                         //td na tr
                         document.getElementById(tr.id).appendChild(td);
-                        
                         num++;
-                        
                     }
-                    
                 }
-
             }
 
-
-            function marca(celula){
+            function marca(celula) {
                     id = celula.id;
                     celula = document.getElementById(id);
                     var style = 'border: 2px solid red; border-radius: 200px;';
-                    
-                    if(!celula.style.border){
+                    if(!celula.style.border) {
                         celula.style = style;
-                    }else{
+                    } else {
                         celula.style.border = '';
                     }
-
                 }
 
-            function limpar(){
+            function limpar() {
                 celula = document.getElementsByTagName('td');
-                for(var x = 0; x<=celula.length; x++){
+                for(var x = 0; x<=celula.length; x++) {
                     celula[x].style.border = '';
                 }
             }
             
             
-            function main(){
+            function main() {
                 //lista_numeros = [1,6,8,11,13,18,22,26,28,29,31,35,36,43,45,46,54,57,58,60];
-                
                 preenche_table(numeros_1,1);
                 preenche_table(numeros_2,2);
-                
             }
-            
         </script>
-
     </head>
 
     <body onload="main()">
-
         <table id="cartela_1">
             <!--table 4x5-->
             <tbody id='conteudo_tabela_1'>
